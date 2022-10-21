a6a28738a8854d003f18d8305dd1a77c96c62f39
Sorbopriyo Roy <78290444+SobujRoySunday@users.noreply.github.com>
2022-10-21 13:23:03 +0530

    Add files via upload

---
+++ print1toNbacktrack.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int i, int n){
+    if(i<1) return;
+    print(i-1, n);
+    cout<<i<<endl;
+}
+
+int main(){
+    int N;
+    cin>>N;
+    print(N, N);
+}
